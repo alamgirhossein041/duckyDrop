2370a1fd92e237608ff9c34b83b255992f7056a9
salluthdev <salluthdev@gmail.com>
2022-11-09 09:45:49 +0700

    Penambahan halaman Connect Wallet

--- pages/layouts/sidebar/sidebar-data.tsx
+++ pages/layouts/sidebar/sidebar-data.tsx
@@ -1,26 +1,22 @@
 export const SidebarData = [
-	{
-		title: "VIP Drop",
-		path: "/vip-drop",
-	},
-	{
-		title: "Airdrop",
-		path: "/airdrop",
-	},
-	{
-		title: "NFT Airdrop",
-		path: "nft-airdrop",
-	},
-	{
-		title: "Approval Menu",
-		path: "/approval",
-	},
-	{
-		title: "Verification",
-		path: "/verification",
-	},
-	{
-		title: "Ducky Hunter",
-		path: "/ducky-hunter",
-	},
+  {
+    title: "VIP Drop",
+    path: "/vip-drop",
+  },
+  {
+    title: "Airdrop",
+    path: "/airdrop",
+  },
+  {
+    title: "Approval Menu",
+    path: "/approval",
+  },
+  {
+    title: "Verification",
+    path: "/verification",
+  },
+  {
+    title: "Ducky Hunter",
+    path: "/ducky-hunter",
+  },
 ];
--- pages/layouts/sidebar/sidebar.tsx
+++ pages/layouts/sidebar/sidebar.tsx
@@ -8,72 +8,76 @@ import styles from "/styles/sidebar.module.scss";
 import Image from "next/image";
 
 export default function Sidebar() {
-	const [isMetamaskInstalled, setIsMetamaskInstalled] = useState<boolean>(false);
-	const [account, setAccount] = useState<string | null>(null);
-	const [ethereumAccount, setEthereumAccount] = useState<string | null>(null);
+  const [isMetamaskInstalled, setIsMetamaskInstalled] =
+    useState<boolean>(false);
+  const [account, setAccount] = useState<string | null>(null);
+  const [ethereumAccount, setEthereumAccount] = useState<string | null>(null);
 
-	useEffect(() => {
-		if ((window as any).ethereum) {
-			setIsMetamaskInstalled(true);
-		}
-	}, []);
+  useEffect(() => {
+    if ((window as any).ethereum) {
+      setIsMetamaskInstalled(true);
+    }
+  }, []);
 
-	async function connectWallet(): Promise<void> {
-		(window as any).ethereum
-			.request({
-				method: "eth_requestAccounts",
-			})
-			.then((accounts: string[]) => {
-				setAccount(accounts[0]);
-			})
-			.catch((error: any) => {
-				alert(`Something went wrong`);
-			});
-	}
+  async function connectWallet(): Promise<void> {
+    (window as any).ethereum
+      .request({
+        method: "eth_requestAccounts",
+      })
+      .then((accounts: string[]) => {
+        setAccount(accounts[0]);
+      })
+      .catch((error: any) => {
+        alert(`Something went wrong`);
+      });
+  }
 
-	// if (ethereumAccount === null) {
-	// 	return (
-	// 		<>
-	// 			{isMetamaskInstalled ? (
-	// 				<>{toast.info(`Please Connect Your Metamask`)}</>
-	// 			) : (
-	// 				<>{toast.warning(`Please Install Your Metamask`)}</>
-	// 			)}
-	// 		</>
-	// 	);
-	// }
+  // if (ethereumAccount === null) {
+  // 	return (
+  // 		<>
+  // 			{isMetamaskInstalled ? (
+  // 				<>{toast.info(`Please Connect Your Metamask`)}</>
+  // 			) : (
+  // 				<>{toast.warning(`Please Install Your Metamask`)}</>
+  // 			)}
+  // 		</>
+  // 	);
+  // }
 
-	return (
-		<>
-			{" "}
-			<div className={styles.sidebar_menu}>
-				<div className={styles.navbar}>
-					<Image
-						src="/image/Logo.svg"
-						width={200}
-						height={50}
-						alt="DuckyDrop Logo"
-					/>
-					<button className={styles.connect_button} onClick={connectWallet}>
-						Connect Wallet
-					</button>
-					<p>{account}</p>
-					{SidebarData.map((item, index) => {
-						return (
-							<div className={styles.navbar_wrapper} key={index}>
-								<div className={styles.icon_wrapper}>
-									<Image src="/image/star.svg" width={20} height={20} alt="star" />
-								</div>
-								<div className={styles.menu_wrapper}>
-									<Link className={styles.menu_links} href={item.path}>
-										<p className={styles.menu_title}>{item.title}</p>
-									</Link>
-								</div>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-		</>
-	);
+  const shortenAddress = (account: any) =>
+    `${account?.slice(0, 5)}...${account?.slice(account?.length - 4)}`;
+  console.log(shortenAddress);
+
+  return (
+    <>
+      <div className={styles.sidebar}>
+        <Image
+          src="/svg/logo.svg"
+          width={200}
+          height={50}
+          alt="Ducky Drop Logo"
+        />
+        <button className={styles.connect_button} onClick={connectWallet}>
+          {account ? shortenAddress(account) : "Connect Wallet"}
+        </button>
+        <div className={styles.menu_wrapper}>
+          {SidebarData.map((item, index) => {
+            return (
+              <div className={styles.menu} key={index}>
+                <Image
+                  src="/image/star.svg"
+                  width={28}
+                  height={28}
+                  alt="Menu"
+                />
+                <Link href={item.path}>
+                  <p>{item.title}</p>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
