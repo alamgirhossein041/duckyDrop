23869b11606cf7002e48b2ed093884bf59135855
candran054 <candran054@gmail.com>
2022-11-14 09:52:21 +0700

    add submenu for sidebar

--- pages/layouts/sidebar/sidebar-data.tsx
+++ pages/layouts/sidebar/sidebar-data.tsx
@@ -1,22 +1,39 @@
 export const SidebarData = [
-  {
-    title: "VIP Drop",
-    path: "/vip-drop",
-  },
-  {
-    title: "Airdrop",
-    path: "/airdrop",
-  },
-  {
-    title: "Approval Menu",
-    path: "/approval",
-  },
-  {
-    title: "Verification",
-    path: "/verification",
-  },
-  {
-    title: "Ducky Hunter",
-    path: "/ducky-hunter",
-  },
+	{
+		title: "VIP Drop",
+		path: "/vip-drop",
+		icon: "lock-circle.svg",
+	},
+	{
+		title: "Airdrop",
+		path: "/airdrop",
+		icon: "/svg/arrow-down.svg",
+		subnav: [
+			{
+				title: "Simple Airdrop",
+				path: "/simple-airdrop",
+			},
+			{
+				title: "Reusable Airdrop",
+				path: "/reusable-airdrop",
+			},
+			{
+				title: "NFT Airdrop",
+				path: "/nft-airdrop",
+			},
+		],
+	},
+	{
+		title: "Approval Menu",
+		path: "/approval",
+	},
+	{
+		title: "Verification",
+		path: "/verification",
+	},
+	{
+		title: "Ducky Hunter",
+		path: "/ducky-hunter",
+		icon: "/image/lock-circle.svg",
+	},
 ];
