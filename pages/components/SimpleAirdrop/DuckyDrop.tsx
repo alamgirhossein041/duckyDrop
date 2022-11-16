bedd5d70fdc520190bb5798d2cab0af17237c5cc
salluthdev <salluthdev@gmail.com>
2022-11-16 09:48:08 +0700

    Penambahan step 3 - simple airdrop

---
+++ pages/components/SimpleAirdrop/DuckyDrop.tsx
@@ -0,0 +1,143 @@
+import styles from "/styles/SimpleAirdrop/DuckyDrop.module.scss";
+import StepWrapper from "../StepWrapper";
+import Button from "../Button";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Image from "next/image";
+
+interface DuckyDropProps {
+  formStep: number;
+}
+
+export default function DuckyDrop({ formStep }: DuckyDropProps) {
+  const { handleSubmit } = useForm();
+  const [transactionDetails, setTransactionDetails] = useState(false);
+  const [transactionDetailsView, setTransactionDetailsView] = useState(false);
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        console.log(data);
+        setTransactionDetails(true);
+      })}
+    >
+      <StepWrapper formStep={formStep} />
+      <div className={styles.airdrop_details}>
+        <div className={styles.detail}>
+          <h2>3</h2>
+          <p>Total recipient</p>
+        </div>
+        <div className={styles.detail}>
+          <h2>1.5</h2>
+          <p>
+            Total <span>STRT</span> to send
+          </p>
+        </div>
+        <div className={styles.detail}>
+          <h2>UNLIMITED</h2>
+          <p>Current Token Approval</p>
+        </div>
+        <div className={styles.detail}>
+          <h2>9,383,425</h2>
+          <p>
+            Your <span>STRT</span> Balance
+          </p>
+        </div>
+      </div>
+      <div className={styles.transaction_info}>
+        <div className={styles.title}>
+          <p>
+            <span>BEP20 - STRT</span> Token Airdrop Transaction Details
+          </p>
+        </div>
+        <div className={styles.row}>
+          <p className={styles.label}>Approximate BNBT cost</p>
+          <p>0.000013 BNBT</p>
+        </div>
+        <div className={`${styles.row} ${styles.bg_blue}`}>
+          <p className={styles.label}>Your BNBT Balance</p>
+          <p>2,98988 BNBT</p>
+        </div>
+        <div className={styles.row}>
+          <p className={styles.label}>Sender Address</p>
+          <p>0x4666a9118E2697226D93155b3f63FE830Fd0b0A1</p>
+        </div>
+        <div className={`${styles.row} ${styles.bg_blue}`}>
+          <p className={styles.label}>Contract Address</p>
+          <p>0x28F22e9Bd908055C2e8b48beb133d2Dd24AD4d1A</p>
+        </div>
+      </div>
+      {transactionDetails == false && (
+        <div className={styles.button_wrapper}>
+          <Button type="submit" color="primary">
+            Duck Your Drop!
+          </Button>
+        </div>
+      )}
+      {transactionDetails && (
+        <>
+          <div className={styles.transaction_status}>
+            <p>DuckyDrop Transaction (1/1)</p>
+            <div className={styles.box}>
+              <div className={styles.address_wrapper}>
+                <p>0x28F22e9Bd908055C2e8b48beb133d2Dd24AD4d1A</p>
+                <Image
+                  src="/svg/copy.svg"
+                  width={24}
+                  height={24}
+                  alt="icon copy"
+                />
+              </div>
+              <div className={styles.icon_success}>
+                <Image
+                  src="/svg/success.svg"
+                  width={24}
+                  height={24}
+                  alt="icon copy"
+                />
+                <p>Success</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.congrats_info}>
+            <p>Congratulation, your Duckdrop successfully dropped!</p>
+          </div>
+          <div className={styles.transaction_details}>
+            <div
+              className={styles.details_toggle}
+              onClick={() => setTransactionDetailsView(!transactionDetailsView)}
+            >
+              <p>View Transaction Details</p>
+              <Image
+                src="/svg/arrow-top.svg"
+                width={20}
+                height={20}
+                className={styles.arrow_top}
+                alt="arrow top"
+              />
+            </div>
+            {transactionDetailsView && (
+              <div className={styles.transaction_details_box}>
+                <div className={styles.row}>
+                  <p className={styles.label}>Duplicate Address</p>
+                  <p>None</p>
+                </div>
+                <div className={`${styles.row} ${styles.bg_blue}`}>
+                  <p className={styles.label}>Matched Address</p>
+                  <p>3 Address</p>
+                </div>
+                <div className={styles.row}>
+                  <p className={styles.label}>Successfully Send</p>
+                  <p>3 Address</p>
+                </div>
+                <div className={`${styles.row} ${styles.bg_blue}`}>
+                  <p className={styles.label}>Failed to Receive</p>
+                  <p>None</p>
+                </div>
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </form>
+  );
+}
--- pages/components/SimpleAirdrop/TransactionApproval.tsx
+++ pages/components/SimpleAirdrop/TransactionApproval.tsx
@@ -2,24 +2,38 @@ import styles from "/styles/SimpleAirdrop/TransactionApproval.module.scss";
 import StepWrapper from "../StepWrapper";
 import Button from "../Button";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 interface TransactionApprovalType {
-  formStep: any;
+  formStep: number;
   prevFormStep: any;
   nextFormStep: any;
 }
+const listOfAddress = [
+  {
+    id: 1,
+    address: "0x4666a9118E2697226D93155b3f63FE830Fd0b0A1",
+    amount: 0.5,
+  },
+  { id: 2, address: "0x49fC7F7E4FFd2a7C6066E51946E58D0Ec6DDaAfB", amount: 20 },
+  {
+    id: 3,
+    address: "0x18353a0536d8304b7c4918da0B43976876627CC1",
+    amount: 3.11,
+  },
+];
 
 export default function TransactionApproval({
+  formStep,
   prevFormStep,
   nextFormStep,
-  formStep,
 }: TransactionApprovalType) {
   const {
-    register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
+
+  const [amountType, setAmountType] = useState("amountToSend");
 
   return (
     <form
@@ -29,9 +43,78 @@ export default function TransactionApproval({
       })}
     >
       <StepWrapper formStep={formStep} />
-      <Button color="primary" onClick={prevFormStep}>
-        Prev
-      </Button>
+      <div className={styles.select_amount_wrapper}>
+        <p>Select amount of tokens to approve:</p>
+        <div className={styles.select_amount}>
+          <div
+            className={`${styles.choice_amount} ${
+              amountType == "amountToSend" && styles.active
+            }`}
+            onClick={() => setAmountType("amountToSend")}
+          >
+            <div className={styles.checkbox}></div>
+            <p>Amount of token to send (1.5 STRT)</p>
+          </div>
+          <div
+            className={`${styles.choice_amount} ${
+              amountType == "totalSupply" && styles.active
+            }`}
+            onClick={() => setAmountType("totalSupply")}
+          >
+            <div className={styles.checkbox}></div>
+            <p>Total supply of the token</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.airdrop_details}>
+        <div className={styles.detail}>
+          <h2>3</h2>
+          <p>Total recipient</p>
+        </div>
+        <div className={styles.detail}>
+          <h2>1.5</h2>
+          <p>
+            Total <span>STRT</span> to send
+          </p>
+        </div>
+        <div className={styles.detail}>
+          <h2>0</h2>
+          <p>Current Token Approval</p>
+        </div>
+        <div className={styles.detail}>
+          <h2>9,383,425</h2>
+          <p>
+            Your <span>STRT</span> Balance
+          </p>
+        </div>
+      </div>
+      <div className={styles.list_of_address}>
+        <div className={styles.title_wrapper}>
+          <p>List of Address (Review)</p>
+          <p>Token Amount</p>
+        </div>
+        <div className={styles.address_box}>
+          {listOfAddress.map((item) => (
+            <div className={styles.row} key={item.id}>
+              <div className={styles.content_left}>
+                <div className={styles.number}>
+                  <p>{item.id}.</p>
+                </div>
+                <p>{item.address}</p>
+              </div>
+              <p>{item.amount}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.button_wrapper}>
+        <Button color="primary" onClick={prevFormStep}>
+          Prev
+        </Button>
+        <Button color="dark" type="submit">
+          Approve
+        </Button>
+      </div>
     </form>
   );
 }
